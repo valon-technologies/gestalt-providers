1e53959b406ff2df8bcc4ae81915f082b6955b08
Hugh Han <hughhan1@gmail.com>
2026-04-10 19:44:37 -0400

    release: bump auth providers to gestaltd alpha.50

--- auth/oidc/provider.go
+++ auth/oidc/provider.go
@@ -20,7 +20,7 @@ import (
 )
 
 const (
-	providerVersion    = "0.0.1-alpha.2"
+	providerVersion    = "0.0.1-alpha.9"
 	defaultSessionTTL  = 24 * time.Hour
 	defaultDisplayName = "SSO"
 	defaultHTTPTimeout = 10 * time.Second
